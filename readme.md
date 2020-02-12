841d12e8e0885807a53437c9155effb6b81a3fdb
mkikuch1 <mkikuchi0211@gmail.com>
2020-02-12 10:34:38 -0500

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+Originally intended to be a take-home assignment for a potential software dev position at MintBean.
+Application made from tutorial: https://github.com/WebDevSimplified/Realtime-Simple-Chat-App to
+improve my web-development skills.
+
+Requirements were the following:
+{requirements here...}
---
+++ script.js
@@ -0,0 +1,35 @@
+  
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => { 
+  appendMessage(`${name} has joined the chat.`);
+})
+
+socket.on('user-disconnected', name => { 
+  appendMessage(`${name} has left the chat.`);
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`${name}: ${message}`);
+  socket.emit('send-message', message);
+  messageInput.value = '';
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
